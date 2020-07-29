f8411ffd5e0260fc3956d5b19ba890134ffb353d
Music Meier <musicm987@gmail.com>
2020-07-29 00:44:25 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_numbers = []
   counter = 0
   while counter < array.length do
-   new_numbers = new_numbers.push(square_root(array[counter]))
+   new_numbers = new_numbers.push(Math.sqrt(array[counter]))
    counter += 1
   end
 new_numbers
